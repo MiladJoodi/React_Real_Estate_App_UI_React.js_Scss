dacdc123eab01331c432941d999e0cb6508774e7
Milad Joodi <winbeta.com@gmail.com>
2024-03-13 21:08:58 +0330

    upa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Navbar from "./components/navbar/Navbar"
 import "./layout.scss"
+import HomePage from "./routes/homePage/homePage"
 
 function App() {
   return (
     <div className="layout">
-      <Navbar />
+
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="content">
+        <HomePage />
+      </div>
+
     </div>
   )
 }
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,11 @@
+import "./homePage.scss"
+
+const HomePage = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default HomePage;
