68ac7d170f1e812684381bfc57170ec09fc92c7a
Milad Joodi <winbeta.com@gmail.com>
2024-03-13 20:40:59 +0330

    play

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,6 +19,22 @@ const Navbar = () => {
             <div className="right">
                 <a href="/">Sign in</a>
                 <a href="/" className="register">Sign up</a>
+
+                {/* Hamburger Icon */}
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="" />
+                </div>
+
+                {/* Mobile Menu */}
+                <div className="menu">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                    <a href="">Agents</a>
+                    <a href="">Sign in</a>
+                    <a href="">Sign up</a>
+                </div>
+
             </div>
         </nav>
     );
