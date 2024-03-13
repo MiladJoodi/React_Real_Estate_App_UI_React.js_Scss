f9e68b853e226ae09481116af82fd6b6dd9e666b
Milad Joodi <winbeta.com@gmail.com>
2024-03-13 21:32:09 +0330

    search

---
+++ src/components/searchBar/searchBar.jsx
@@ -0,0 +1,11 @@
+import "./searchBar.scss"
+
+const searchBar = () => {
+    return (
+        <div className="search">
+            Enter
+        </div>
+    );
+}
+
+export default searchBar;
--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -2,8 +2,21 @@ import "./homePage.scss"
 
 const HomePage = () => {
     return (
-        <div>
-            Enter
+        <div className="homePage">
+            {/* Home -> Hero -> Left */}
+            <div className="textContainer">
+                <div className="wrapper">
+                    <h1 className="title">
+                        Find Real Estate & Get Your Dream Place
+                    </h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis consectetur, impedit, ipsam illo reiciendis id reprehenderit, praesentium autem repellendus similique quia explicabo quidem quam excepturi facere omnis sint! At.</p>
+                </div>
+            </div>
+
+            {/* Home -> Hero -> Left */}
+            <div className="imgContainer">
+                <img src="/bg.png" alt="" />
+            </div>
         </div>
     );
 }
