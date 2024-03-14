39724d4293b714cbd560cee63ecef5462e2b8ce8
Milad Joodi <winbeta.com@gmail.com>
2024-03-14 17:03:01 +0330

    finish routes

---
+++ src/routes/singlePage/singlePage.jsx
@@ -0,0 +1,11 @@
+import "./singlePage.scss"
+
+const searchBar = () => {
+    return (
+        <div className="searchBar">
+            Enter
+        </div>
+    );
+}
+
+export default searchBar;
