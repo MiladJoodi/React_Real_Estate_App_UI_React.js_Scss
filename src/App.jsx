51fbc23d227d987373f928d20d635957059ac598
Milad Joodi <winbeta.com@gmail.com>
2024-03-14 16:19:43 +0330

    00

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,32 @@
-import Navbar from "./components/navbar/Navbar"
-import "./layout.scss"
 import HomePage from "./routes/homePage/homePage"
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
 } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
+import Layout  from "./routes/layout/layout";
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <HomePage />
-      ),
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        }
+      ]
     },
-    {
-      path: "/list",
-      element: <ListPage />,
-    },
+    
   ]);
 
   return (
-
-
-    // <div className="layout">
-
-    //   <div className="navbar">
-    //     <Navbar />
-    //   </div>
-
-    //   <div className="content">
-    //     <HomePage />
-    //   </div>
-
-    // </div>
-
     <RouterProvider router={router} />
   )
 }
