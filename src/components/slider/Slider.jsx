67823b9ac9a110777b97c5045044066803d2c0ea
Milad Joodi <winbeta.com@gmail.com>
2024-03-15 14:17:17 +0330

    up

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,11 @@
+import "./slider.scss"
+
+const Slider = () => {
+    return (
+        <div className="slider">
+            slider
+        </div>
+    );
+}
+
+export default Slider;
--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,9 +1,35 @@
+import Slider from "../../components/slider/Slider";
 import "./singlePage.scss"
+import { singlePostData, userData } from "../../lib/dummydata";
 
 const SinglePage = () => {
     return (
-        <div className="">
-            SinglePage
+        <div className="singlePage">
+            <div className="details">
+                <div className="wrapper">
+                    <Slider images={singlePostData.images} />
+                    <div className="info">
+                        <div className="top">
+                            <div className="post">
+                                <h1>{singlePostData.title}</h1>
+                                <div className="address">
+                                    <img src="/pin.png" alt="" />
+                                    <span>{singlePostData.address}</span>
+                                </div>
+                                <div className="price">$ {singlePostData.price}</div>
+                            </div>
+                            <div className="user">
+                                <img src={userData.img} alt="" />
+                                <span>{userData.name}</span>
+                            </div>
+                        </div>
+                        <button className="bottom"></button>
+                    </div>
+                </div>
+            </div>
+            <div className="features">
+                <div className="wrapper"></div>
+            </div>
         </div>
     );
 }
